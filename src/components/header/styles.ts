c2dde8ab0a62b81283a2397130d2187378af0e2b
Vinicius Dias <vinicius-mdias@hotmail.com>
2024-05-06 15:13:54 -0300

    feat: folders and initial sctructure created.

---
+++ src/components/header/styles.ts
@@ -0,0 +1,29 @@
+import { styled } from 'styled-components'
+
+export const Header = styled.header`
+	display: flex;
+`
+
+
+export const HeaderContent = styled.div`
+	display: flex;
+`
+
+
+export const Logo = styled.div`
+	display: flex;
+`
+
+
+export const Menu = styled.ul`
+	display: flex;
+`
+
+
+export const MenuItem = styled.li`
+	display: flex;
+`
+
+export const SocialContent = styled.div`
+	display: flex;
+`
---
+++ src/components/intro/index.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import * as S from './styles'
+
+export function Hero() {
+
+  return (
+    <S.HeroContainer>
+      <h1>Hello Hero!</h1>
+    </S.HeroContainer>
+  );
+}
